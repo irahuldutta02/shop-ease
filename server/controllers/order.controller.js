b6e431d15a8d4f49724dcad42411bc6a6be030d4
irahuldutta02 <78687135+irahuldutta02@users.noreply.github.com>
2024-03-28 01:44:05 +0530

    feat: get orders route for admin

--- server/controllers/order.controller.js
+++ server/controllers/order.controller.js
@@ -95,4 +95,45 @@ const getUserOrders = asyncHandler(async (req, res) => {
   }
 });
 
-export { addOrderItems, getOrderById, getUserOrders };
+const getOrders = asyncHandler(async (req, res) => {
+  try {
+    const orders = await Order.find({}).populate("user", "id name email");
+    res.status(200).json(orders);
+  } catch (error) {
+    res.status(400);
+    throw new Error("Error Fetching Orders");
+  }
+});
+
+const updateOrderToDelivered = asyncHandler(async (req, res) => {
+  try {
+    const order = await Order.findById(req.params.id);
+
+    if (order) {
+      order.isDelivered = true;
+      order.deliveredAt = Date.now();
+
+      const updatedOrder = await order.save();
+
+      res.json({
+        status: 200,
+        message: "Order delivered",
+        data: updatedOrder,
+      });
+    } else {
+      res.status(404);
+      throw new Error("Order not found");
+    }
+  } catch (error) {
+    res.status(400);
+    throw new Error(error.message);
+  }
+});
+
+export {
+  addOrderItems,
+  getOrderById,
+  getUserOrders,
+  getOrders,
+  updateOrderToDelivered,
+};
--- server/routes/oder.routes.js
+++ server/routes/oder.routes.js
@@ -2,14 +2,17 @@ import express from "express";
 import {
   addOrderItems,
   getOrderById,
+  getOrders,
   getUserOrders,
+  updateOrderToDelivered,
 } from "../controllers/order.controller.js";
-import { protect } from "../middleware/auth.middleware.js";
+import { protect, admin } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
-router.route("/").post(protect, addOrderItems);
+router.route("/").post(protect, addOrderItems).get(protect, admin, getOrders);
 router.route("/user-orders").get(protect, getUserOrders); // sequence is important
 router.route("/:id").get(protect, getOrderById);
+router.route("/deliver/:id").patch(protect, admin, updateOrderToDelivered);
 
 export default router;

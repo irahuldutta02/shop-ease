9a7974253bb94be30b201b193d253ff9d82af595
irahuldutta02 <78687135+irahuldutta02@users.noreply.github.com>
2024-03-29 13:16:59 +0530

    feat: dow it is possible to install the client as pwa

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,50 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
+      manifest: {
+        name: "ShopEase",
+        short_name: "ShopEase",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "pwa-64x64.png",
+            sizes: "64x64",
+            type: "image/png",
+          },
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "maskable-icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+    }),
+  ],
   server: {
     port: 3000,
     proxy: {
       // "/api": "http://localhost:5000", // for development
-      "/api": "https://shop-ease-a7ya.onrender.com",
+      "/api": "https://shop-ease-a7ya.onrender.com", // for production
     },
   },
 });
